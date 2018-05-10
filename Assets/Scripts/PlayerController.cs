8d1cc9cc433bf438bcebe4b85b2c1c839cff0538
surue <nicolas06062000@hotmail.com>
2018-05-10 17:37:05 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5;
+
+    Rigidbody2D body;
+
+    Vector2 moveVelocity;
+
+    Camera mainCamera; 
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        mainCamera = FindObjectOfType<Camera>();
+	}
+
+    void FixedUpdate() {
+        body.velocity = moveVelocity;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        moveVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+    }
+}
